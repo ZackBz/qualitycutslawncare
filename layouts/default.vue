b627605da427ee93671063cf601658b7a1b4105a
Unknown <zackbaldwin148@gmail.com>
2017-12-03 22:44:42 -0500

    basic setup

---
+++ layouts/default.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <section>
+    <nav class="header green">
+
+      <div class="left">
+        <a href="#" class="width-phablet-12"><h4 class="margin-0">Quality Cuts Lawn Care</h4></a>
+      </div>
+
+      <div class="center">
+        <a href="#" class="width-phablet-12">Home</a>
+        <a href="#" class="width-phablet-12">Contact</a>
+        <a href="#" class="width-phablet-12 right">About</a>
+      </div>
+    </nav>
+    </section>
+    <nuxt/>
+  </div>
+</template>
+
+<style lang="scss">
+@import '~assets/main.scss';
+/*html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}*/
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template lang="html">
+  <div>
+    <section>
+      <div class="body--giant blue">
+        <div class="center">
+          <h3 class="margin-03 margin-left-0">Banner</h3>
+          <p>Default size</p>
+        </div>
+      </div>
+    </section>
+
+    <div class="columns">
+      <div class="column width-tablet-5">
+          <div class="card red">
+              <p>I am a column</p>
+          </div>
+      </div>
+      <div class="column width-phone-2">
+        <div class="card yellow">
+            <p>I am </p>
+        </div>
+      </div>
+
+      <div class="column width-phone-2">
+        <section>
+          <div class="body--large">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </section>
+        <div class="card purple">
+            <p>I am </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="css">
+</style>
