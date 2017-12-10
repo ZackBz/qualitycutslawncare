63611587f01798fb2962ce5669b6e42aeb0e8a21
Unknown <zackbaldwin148@gmail.com>
2017-12-09 19:32:53 -0500

    fixes

--- pages/index.vue
+++ pages/index.vue
@@ -2,19 +2,18 @@
   <div>
     <slide-show seconds="6" :images="['/images/grass.jpg', '/images/lawnmower.jpg', '/images/grass2.jpg']"/>
 
-    <div class="columns is-multiline" >
+    <div class="columns" >
       <!-- <div class="column is-three-fifths image is-hidden-touch"></div> -->
-      <div class="is-full columns">
-        <div class="column is-two-fifths is-hidden-touch parallax" style="background: url('/images/leavesongrass.jpg')"></div>
-        <div class="column is-three-fifths is-full-touch text">
-          <div class="is-size-2 has-text-weight-bold">About</div>
-          <p class="is-size-5">We are committed to enhancing our clients’ most important investment by exceeding their lawn service needs with reliability and value. Imagine quality family time, rest and relaxation, and leaving the dirty work up to us. Your business is just as important as ours. Like any other turf, the lawn around your business requires attention and service. Our flexibility and ability to customize a lawn care program for your property allows your business to make a great first impression.</p>
 
+        <div class="column is-two-fifths is-hidden-touch parallax" style="background: url('/images/leavesongrass.jpg')"></div>
+        <div class="column is-three-fifths is-full-touch text center">
+          <div class="is-size-2 has-text-weight-bold">About</div>
+          <p class="is-size-5 max">We are committed to enhancing our clients’ most important investment by exceeding their lawn service needs with reliability and value. Imagine quality family time, rest and relaxation, and leaving the dirty work up to us. Your business is just as important as ours. Like any other turf, the lawn around your business requires attention and service. Our flexibility and ability to customize a lawn care program for your property allows your business to make a great first impression.</p>
           <div class="is-full is-hidden-desktop image" id="about-touch-image" style="background: url('/images/leavesongrass.jpg')"></div>
         </div>
-      </div>
+    </div>
 
-
+    <div class="columns">
       <div class="column is-desktop text" style="background-image: url('/images/drone.jpg');">
         <div id="services" class="column is-half">
           <div class="is-size-2 has-text-weight-bold">Services</div>
@@ -27,11 +26,12 @@
           </ul>
         </div>
       </div>
+    </div>
 
-      <div class="is-full columns">
+      <div class="columns">
         <div class="column is-three-fifths is-full-touch text">
           <div class="is-size-2 has-text-weight-bold">section</div>
-          <p class="is-size-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum./p>
+          <p class="is-size-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
           <div class="is-full is-hidden-desktop image" style="background: url('/images/leavesongrass.jpg')"></div>
         </div>
@@ -67,5 +67,9 @@ export default {
 #about-touch-image {
   margin-top: 3rem;
   height: 5rem;
+}
+
+.max {
+  max-width: 300px;
 }
 </style>
