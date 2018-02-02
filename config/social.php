961fde2ea0bd36588845e4c1ff87dbb6e6074c3d
Unknown <zackbaldwin148@gmail.com>
2018-02-02 13:41:19 -0500

    social media

--- config/social.php
+++ config/social.php
@@ -1,7 +1,7 @@
 <?php
 return [
-  "Facebook"    => "",
-  "Instagram"   => "",
+  "Facebook"    => "https://www.facebook.com/Quality-Cuts-Lawn-Care-1982196262029201/",
+  "Instagram"   => "https://www.instagram.com/qualitycuts_lawncare/",
   "YouTube"     => "",
   "Twitter"     => "",
   "Reddit"      => "",
