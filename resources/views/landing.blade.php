7ed43f6d1bcfd78c5cae485b7a1f58ef8ee28f1d
Unknown <zackbaldwin148@gmail.com>
2018-02-02 21:19:17 -0500

    fixed spelling

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -10,7 +10,7 @@
 <div class="small-sect">
     <h1 class="title">About</h1>
     <p>Quality Cuts is independently owned and operated offering the best lawncare to both commercial and residential properties. Quality Cuts is registered, insured and certainly qualified to fulfill all of your property maintance needs. Call, email, or
-      <a href="#contact" class="scrolling">submit a request</a> to set up a fast, free estimate. </p>
+      <a href="#contact" class="scrolling">submit a request</a> to set up a fast, free estimate.</p>
 </div>
 @endcomponent
 
@@ -23,8 +23,8 @@
   'id' => 'lawnmowing'
 ])
 <div class="small-sect">
-<h1 class="title">Lawn mowing</h1>
-<p>When you let Quality Cuts maintain your lawn care needs, you're not just investing in a beautiful looking lawn, you're also buying your free time back! Imagine coming home to a perfectly manicured lawn and being able to enjoy summer evenings doing as you please. Services may include mowing, bagging grass, fertilization, trimming shrubs/bushes, edging walks/drives and all services including blowing off all excess grass from walks/drives/etc.</p>
+<h1 class="title">Lawn Mowing</h1>
+<p>When you let Quality Cuts maintain your lawn care needs, you're not just investing in a beautiful looking lawn, you're also buying your free time back! Imagine coming home to a perfectly manicured lawn and being able to enjoy summer evenings doing as you please. Services may include mowing, bagging grass, fertilization, trimming shrubs/bushes, edging walks/drives and all services include blowing off all excess grass from walks/drives/etc.</p>
 </div>
 @endcomponent
 
@@ -35,7 +35,7 @@
 ])
 <div class="small-sect">
 <h1 class="title">Mulch & Stone Delivery</h1>
-<p>Let us shovel it for you! Nothing helps add curb appeal to your property then freshening up of mulch or decorative rock delivered and installed.</p>
+<p>Let us shovel it for you! Nothing helps add curb appeal to your property then freshening up of mulch or decorative rocks which are delivered and installed for your convenience.</p>
 </div>
 
 @endcomponent
@@ -46,7 +46,7 @@
 ])
 <div class="small-sect">
 <h1 class="title">Spring & Fall Cleanup</h1>
-<p>Leavs in the fall? Debris in the spring? Whatever cleaning up your lawn needs, we can make it look its best.</p>
+<p>Leaves in the fall? Debris in the spring? Whatever cleaning up your lawn needs, we can make it look its best.</p>
 </div>
 
 @endcomponent
